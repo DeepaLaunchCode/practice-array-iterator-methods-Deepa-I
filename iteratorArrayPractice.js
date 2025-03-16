33d34378b0cd379bbb5be6c9423d8927690e742f
DeepaLaunchCode <sonaliforamit@gmail.com>
2025-03-15 20:33:49 -0500

    first commit Practice: Array Iterator Methods

--- iteratorArrayPractice.js
+++ iteratorArrayPractice.js
@@ -1,29 +1,44 @@
-// Task 1: Decode the Following Reversed Messages
-function reverseMessage(message) {
-    return message.split('').reverse().join('');
-}
+// Task 1: Using forEach()
+let favoriteCities = ["Mumbai", "Paris", "New York", "Sydney", "Chicago"];
 
-let message1 = reverseMessage(" !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC");
-let message2 = reverseMessage("!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB");
-let message3 = reverseMessage("!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY");
-let message4 = reverseMessage(".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ");
+favoriteCities.forEach(city => console.log(city.toUpperCase()));
+/*output: task 1
+MUMBAI
+PARIS
+NEW YORK
+SYDNEY
+CHICAGO 
+*/
 
-console.log("Decoded Messages:");
-console.log(message1);
-console.log(message2);
-console.log(message3);
-console.log(message4);
+// Task 2: Transforming with map()
+let numbers = [1, 2, 3, 4, 5];
+let squares = numbers.map(num => num ** 2);
 
-// Task 2: Write Your Own Reverse Messages
-let myMessage1 = "Launchcode is my way to learn new skill !";
-let myMessage2 = "You should meditate for at least 20 min.";
-let myMessage3 = "Success comes from consistency and hard work.";
+console.log(squares);
+/*output: task 2
+[ 1, 4, 9, 16, 25 ]
+ */
 
-let reversedMyMessage1 = reverseMessage(myMessage1);
-let reversedMyMessage2 = reverseMessage(myMessage2);
-let reversedMyMessage3 = reverseMessage(myMessage3);
 
-console.log("\nReversed Messages:");
-console.log(reversedMyMessage1);
-console.log(reversedMyMessage2);
-console.log(reversedMyMessage3);
+
+// Task 3: Filtering with filter()
+let scores = [85, 42, 90, 75, 30, 100];
+let highScores = scores.filter(score => score >= 80);
+
+console.log(highScores);
+/*output: task 3
+[ 85, 90, 100 ] 
+ */
+
+// Task 4: Finding with find() and findIndex()
+let favoriteFood = ["Sandwitch", "Biryani", "Pasta", "Cake", "Burger", "Pizza"];
+let firstLongFood = favoriteFood.find(food => food.length > 4);
+let indexLongFood = favoriteFood.findIndex(food => food.length > 4);
+
+console.log("First food with more than 4 letters:", firstLongFood);
+console.log("Index of that food:", indexLongFood);
+
+/*output: task 4
+// First food with more than 4 letters: Sandwitch
+// Index of that food: 0
+*/
